4c4d3f1eb8f5df6111c7da350cda76368830dc61
Marnin-A <msmaudu@gmail.com>
2023-06-20 12:50:54 +0100

    Add signin and login components, and page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import "./App.css";
 import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/SignUp";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    { path: "/", element: <Login /> },
+    { path: "home", element: <Home /> },
+    { path: "signup", element: <Signup /> },
+  ]);
   return (
     <div className=" h-screen flex flex-col items-center justify-center">
-      <Home />
+      <RouterProvider router={router} />
     </div>
   );
 }
