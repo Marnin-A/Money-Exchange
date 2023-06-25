c6e519efa6397ea2b6b96952196f08d1f1087b5c
Marnin-A <msmaudu@gmail.com>
2023-06-25 17:24:30 +0100

    Finish user auth

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,6 +21,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const googleAuth = new GoogleAuthProvider();
+export const db = getFirestore(app);
 const analytics = getAnalytics(app);
 
 export default app;
